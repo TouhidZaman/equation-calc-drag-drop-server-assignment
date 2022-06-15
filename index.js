cfd556ad3b16573b61923a347878a9b56c14568f
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-06-15 16:33:21 +0600

    equation-calc-drag-drop-server initialized

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello from  Equation calc drag drop server");
+});
+
+//listen to port
+app.listen(port, () => {
+    console.log("Listening to port", port);
+});
